7f5a04cf7a866f3a2b1331a0b660bacca8ee4b6a
smmu94 <stephaniemmu@gmail.com>
2026-02-19 12:36:36 -0500

    Add initial project setup

---
+++ src/app/layout.tsx
@@ -0,0 +1,35 @@
+import "@/styles/globals.css";
+import type { Metadata } from "next";
+import { Geist, Inter } from "next/font/google";
+
+const inter = Inter({
+    variable: "--font-inter",
+    subsets: ["latin"],
+});
+
+const geistSans = Geist({
+    variable: "--font-geist-sans",
+    subsets: ["latin"],
+});
+
+export const metadata: Metadata = {
+    title: "User Management App",
+    description:
+        "A clean and professional CRUD application to manage users with React, Next.js, and TypeScript",
+};
+
+export default function RootLayout({
+    children,
+}: Readonly<{
+    children: React.ReactNode;
+}>) {
+    return (
+        <html lang="en">
+            <body
+                className={`${geistSans.variable} ${inter.variable} antialiased bg-background text-foreground px-6 py-12`}
+            >
+                {children}
+            </body>
+        </html>
+    );
+}
